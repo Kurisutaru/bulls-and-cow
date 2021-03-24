0e008994fff3879d8a40996bcad550ff5c181fb0
Varenchinusu Michael <Me@Kurisutaru.net>
2021-03-24 10:11:47 +0700

    [1.0] BullsAndCows
    Numeric Edition

---
+++ BullsAndCows/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BullsAndCows
+{
+  class Program
+  {
+    static int bullCount = 0;
+    static int cowCount = 0;
+    static Random random = new Random();
+    static bool win = false;
+
+    static IList<string> secretKey = new List<string>();
+    static IList<string> inputKey = new List<string>();
+    static IList<InputHistory> inputHistory = new List<InputHistory>();
+
+    static void Main(string[] args)
+    {
+      while(secretKey.Count < 4)
+      {
+        string tmp = Convert.ToString(random.Next(0, 9));
+        if(!secretKey.Contains(tmp))
+        {
+          secretKey.Add(tmp);
+        }
+      }
+
+      while(!win)
+      {
+        Console.Clear();
+        PrintHistory();
+
+        //Give Space
+        Console.Write("Your Guess [4 Digit] ? ");
+        string tmp = Console.ReadLine();
+
+        if(!string.IsNullOrEmpty(tmp) && tmp.Length == 4 && !NonDuplicateCheck(tmp))
+        {
+          EngineCheck(tmp);
+
+          if(cowCount == 4)
+          {
+            win = true;
+            Console.Clear();
+            PrintHistory();
+            Console.WriteLine("You win !");
+            Console.Write("Press any key to exit . . .");
+            Console.Read();
+          }
+        }
+      }
+    }
+
+    public static void EngineCheck(string input)
+    {
+      //Set Default
+      bullCount = 0;
+      cowCount = 0;
+      inputKey.Clear();
+
+      for (int i = 0; i < input.Length; i++)
+      {
+        inputKey.Add(input[i].ToString());
+      }
+
+      for (int i = 0; i < secretKey.Count; i++)
+      {
+        if(secretKey[i] == inputKey[i])
+        {
+          cowCount++;
+        }
+        else if (secretKey.Contains(inputKey[i]))
+        {
+          bullCount++;
+        }
+      }
+
+      inputHistory.Add(new InputHistory() { input = input, result = $"Cow : {cowCount}, Bull : {bullCount}" });
+
+    }
+
+    public static void PrintHistory()
+    {
+      if (inputHistory.Count > 0)
+      {
+        Console.WriteLine($"Input History");
+        Console.WriteLine($"=============");
+        for (int i = 0; i < inputHistory.Count; i++)
+        {
+          Console.WriteLine($"{i+1} | {inputHistory[i].input} | {inputHistory[i].result}");
+        }
+        Console.WriteLine("=============");
+      }
+    }
+
+    public static bool NonDuplicateCheck(string input)
+    {
+      var set = new HashSet<char>();
+      return input.Any(x => !set.Add(x));
+    }
+  }
+
+  class InputHistory
+  {
+    public string input { get; set; }
+    public string result { get; set; }
+  }
+}
